c10b956a27f7b9bbe2292309ca20e9176297e310
marutoraman <33813153+marutoraman@users.noreply.github.com>
2021-11-16 14:11:46 +0900

    Create item.py

---
+++ my_app/models/item.py
@@ -0,0 +1,28 @@
+from sell.models.item_stock import ItemStockModel
+from sell.models.mercari_category import MercariCategoryModel
+from django.db import models
+from imagekit.models import ImageSpecField, ProcessedImageField
+from imagekit.processors import ResizeToFill
+import ulid
+from ulid.api import default
+from users.models import User
+from app.settings import S3_ITEM_IMAGE_PATH
+from app.common import ULIDFileName
+from app.const import CHOICE
+
+# 商品データモデル
+class ItemModel(models.Model):
+    # idはデフォルトではなく、ulidを生成するようにした方がセキュリティ上良い。デフォルトのintの連番だと容易に推測されてしまう
+    id = models.CharField(max_length=32, default=ulid.new, primary_key=True)
+    title = models.CharField("商品名", max_length=128, null=False)
+    description = models.TextField("説明", null=True, default="",blank=True)
+
+    # 作成日時、更新日時は基本的に全てのテーブルにあった方がよい
+    created_at = models.DateTimeField("登録日時", auto_now_add=True)
+    updated_at = models.DateTimeField("更新日時", auto_now=True)
+
+    # userは外部キーで紐づける
+    user = models.ForeignKey(User, db_column="user_id", on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        db_table='mercari_item' # table名は明示的に指定する方がわかりやすい
